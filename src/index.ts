b8f98032b9784b3c0e6e02208b40f2158bb45ce8
gandak <gandak0930@gmail.com>
2025-02-13 23:17:37 +0800

    edit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+
 import { userRoute } from "./routes/user.routes";
 import { productRoute } from "./routes/product.routes";
 import bodyParser from "body-parser";
@@ -9,6 +10,7 @@ import { basketRoute } from "./routes/basket.routes";
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/user", userRoute);
